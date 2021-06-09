e2ea7da65793fd0e1b63d8b36d675fa3f64ce362
BIKASH DASH <vikashdash004@gmail.com>
2021-06-09 22:45:08 +0530

    dynamic value to the template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
 export default function App() {
+  const title = 'welcome to new blog';
+  const likes = 50;
+  const person = {name: "vikash",age:40}
+  const link = "http://google.com"
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>likes {likes} times</p>
+        <pre>{JSON.stringify(person)}</pre>
+        <p>{Math.random()*10}</p>
+        <a href={link}>Google</a>
       </div>
     </div>
   );
