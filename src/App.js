13a3c5ea65cec5f2766c34afd1c2a1d4c499734d
BIKASH DASH <vikashdash004@gmail.com>
2021-06-09 22:57:57 +0530

    added multiple components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 export default function App() {
   const title = 'welcome to new blog';
   const likes = 50;
-  const person = {name: "vikash",age:40}
+  const person = { name: "vikash", age: 40 }
   const link = "http://google.com"
   return (
     <div className="App">
       <div className="content">
+        <Navbar />
         <h1>{title}</h1>
+        <Home/>
         <p>likes {likes} times</p>
         <pre>{JSON.stringify(person)}</pre>
-        <p>{Math.random()*10}</p>
+        <p>{Math.random() * 10}</p>
         <a href={link}>Google</a>
       </div>
     </div>
