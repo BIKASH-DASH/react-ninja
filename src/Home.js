f24c88ef0e3fc75c05868179f52d2f3743a9b9c0
BIKASH DASH <vikashdash004@gmail.com>
2021-06-10 22:34:34 +0530

    done click event

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,19 @@
 const Home = () => {
-    return ( 
+    const hendelClicK = () => {
+        console.log("hello ninja");
+    }
+
+    const hendelClickAgain = (name,event) =>{
+        console.log('hello '+name,event)
+    }
+
+    return (
         <div className="Home">
             <h2>Home page</h2>
+            <button onClick={hendelClicK} >Click me</button>
+            <button onClick={(e)=>hendelClickAgain("helloBikash",e)}>Click again</button>
         </div>
-     );
+    );
 }
- 
+
 export default Home;
