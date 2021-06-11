86f7896c9ae3f0aee770d7a7fbd1bf20a7e4eae2
BIKASH DASH <vikashdash004@gmail.com>
2021-06-11 21:50:36 +0530

    outputing list

--- src/App.js
+++ src/App.js
@@ -3,19 +3,16 @@ import Home from "./Home";
 
 export default function App() {
   const title = 'welcome to new blog';
-  const likes = 50;
-  const person = { name: "vikash", age: 40 }
-  const link = "http://google.com"
   return (
     <div className="App">
       <div className="content">
         <Navbar />
         <h1>{title}</h1>
         <Home/>
-        <p>likes {likes} times</p>
+        {/* <p>likes {likes} times</p>
         <pre>{JSON.stringify(person)}</pre>
         <p>{Math.random() * 10}</p>
-        <a href={link}>Google</a>
+        <a href={link}>Google</a> */}
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,23 @@
-import react, { useState } from "react";
+import  { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState('mirio');
-    const [age, setAge] = useState('age')
-    const hendelClicK = () => {
-        console.log("hello ninja");
-        setName('dash');
-        setAge ('23')
-    }
-
-
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return (
         <div className="Home">
-            <h2>Home page</h2>
-            <p>name :{name}</p>
-            <p>age : {age}</p>
-            <button onClick={hendelClicK} >Click me</button>
+            <h2>list blog</h2>
+            {blogs.map((blog,index)=>{
+                console.log(blog,index)
+                return (<div key={index} className="blog">
+                            <h1>{blog.title}</h1>
+                            <p>{blog.body}</p>
+                            <a href={"#"}>{blog.author}</a>
+                </div>)
+            })}
         </div>
     );
 }
