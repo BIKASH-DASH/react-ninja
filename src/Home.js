5ec04cc3d62262f50fe26e21eea29db0f502c3d2
BIKASH DASH <vikashdash004@gmail.com>
2021-06-12 09:05:50 +0530

    create json server

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
-
+    const [Name,setName] = useState('raja')
     const deleteThisBlog = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -15,11 +15,13 @@ const Home = () => {
 
     useEffect(()=>{
         console.log('use effect run');
-    })
+        
+    },[Name])
 
     return (
         <div className="Home">
-
+            <p>{Name}</p>
+            <button onClick={()=>setName('babu')}>change Name</button>
 
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={'theSecondBlog'} deleteThisBlog={deleteThisBlog} />
         </div>
